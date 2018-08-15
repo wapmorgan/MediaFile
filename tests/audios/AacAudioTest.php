747a7ca91555780c020b4c1a946dff708ddc035f
wapmorgan <wapmorgan@gmail.com>
2018-08-15 23:29:10 +0300

    update tests

--- tests/audios/AacAudioTest.php
+++ tests/audios/AacAudioTest.php
@@ -8,8 +8,10 @@ class AacAudioTest extends PHPUnit_Framework_TestCase
 		$file = MediaFile::open(FIXTURES_DIR.'/audio/audio_1.aac');
 		$this->assertInstanceOf('wapmorgan\MediaFile\MediaFile', $file);
 		$this->assertTrue($file->isAudio());
-		$audio = $file->getAudio();
-		$this->assertInstanceOf('wapmorgan\MediaFile\AudioAdapter', $audio);
+
+        // checking audio interfac
+        $audio = $file->getAudio();
+		$this->assertInstanceOf('wapmorgan\MediaFile\Adapters\AudioAdapter', $audio);
 		$this->assertEquals(34, $audio->getLength(), '', 2);
 		$this->assertEquals(44100, $audio->getSampleRate());
 		$this->assertEquals(2, $audio->getChannels());
